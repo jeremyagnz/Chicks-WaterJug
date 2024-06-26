d582838529b118f754f295112905459cde8dddad
jeremy <j.arias@motorcredito.com.do>
2024-06-26 16:10:57 -0400

    Adding Logic of Water Jug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WaterJugComponent } from './water-jug/water-jug.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+import { HeaderChallengeComponent } from './header-challenge/header-challenge.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WaterJugComponent
+    WaterJugComponent,
+    HeaderChallengeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/header-challenge/header-challenge.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-challenge',
+  templateUrl: './header-challenge.component.html',
+  styleUrls: ['./header-challenge.component.css']
+})
+export class HeaderChallengeComponent {
+
+}
