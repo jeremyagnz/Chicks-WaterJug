43bc20d00ae0188e5ea0bac388b8f753a009ec90
jeremy <j.arias@motorcredito.com.do>
2024-06-26 17:02:05 -0400

    Implements MediaQueries and loading

--- src/app/water-jug/water-jug.component.ts
+++ src/app/water-jug/water-jug.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { WaterJugLogicService } from '../service/water-jug-logic.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import {  viewSolvability } from '../enums/message.enum';
 
 @Component({
   selector: 'app-water-jug',
@@ -12,14 +13,21 @@ export class WaterJugComponent {
   capacityX!: number;
   capacityY!: number;
   target!: number;
+  loading = false;
 
   constructor(private waterJugLogicService: WaterJugLogicService, private snackBar: MatSnackBar) { }
 
   checkSolvability() {
+    this.loading = true;
     const result = this.waterJugLogicService.isSolvable(this.capacityX, this.capacityY, this.target);
-    /* Add Enums */
-    const message = result ? 'Yes, it is possible!' : 'No, it is not possible.';
-    this.showToast(message);
+    const message = result ? viewSolvability.isSolvable : viewSolvability.notSolvable;
+
+    setTimeout(() => {
+      this.loading = false;
+
+      this.showToast(message);
+    }, 2000);
+
   }
 
   showToast(message: string) {
