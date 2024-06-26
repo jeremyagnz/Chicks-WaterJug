98612dce0a9d9e985bcd489ab2c84e3a492c2054
jeremy <j.arias@motorcredito.com.do>
2024-06-25 23:26:27 -0400

    Adding Services, component and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WaterJugComponent } from './water-jug/water-jug.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:WaterJugComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/water-jug/water-jug.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-water-jug',
+  templateUrl: './water-jug.component.html',
+  styleUrls: ['./water-jug.component.css']
+})
+export class WaterJugComponent {
+
+}
